d09555c160a1e4acfde226b3683c4c032f0cd9be
Angelo Queiroz <angelo.queiroz@yapily.com>
2023-05-15 23:04:59 +0100

    email template fix

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import axios from 'axios'
 import mg from '../../services/mailgun'
 import { render } from '@react-email/render'
-import ContactTemplate from '../../components/Emails/ContactTemplate'
+import ContactTemplate from '../../emails/ContactTemplate'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
