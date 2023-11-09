6aa21ff91cc235b9c9945fc43a64ed3c81640b55
Angelo Queiroz <angelo.queiroz@yapily.com>
2023-11-09 01:22:35 +0000

    Mailgun response await

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -43,26 +43,24 @@ export async function POST(request: Request) {
             message: data.message,
           }),
         )
-        mg.messages
-          .create(process.env.MAILGUN_DOMAIN || 'mail.angeloqueiroz.com', {
+        const res = await mg.messages.create(
+          process.env.MAILGUN_DOMAIN || 'mail.angeloqueiroz.com',
+          {
             from: 'Angelo Queiroz Website <noreply@angeloqueiroz.com>',
             to: ['contato@angeloqueiroz.com'],
             subject: 'Angelo Queiroz | Website Contact Form',
             text: content,
             html: htmlContent,
-          })
-          .then(() =>
-            Response.json(
-              {
-                status: 'success',
-                message: 'E-mail successfully sent!',
-              },
-              { status: 200 },
-            ),
-          )
-          .catch((err: any) =>
-            Response.json({ status: 'failure', message: err }, { status: 400 }),
-          )
+          },
+        )
+        console.log(res)
+        return Response.json(
+          {
+            status: 'success',
+            message: 'E-mail successfully sent!',
+          },
+          { status: 200 },
+        )
       } else {
         return Response.json(
           { status: 'failure', message: 'Failed ReCaptcha Score' },
